d57e869b7e67fdcbd06346e26c4a2d11a630686d
lagalapa <lagalapa@gmail.com>
2016-06-01 16:22:12 +0000

    Make methods in Train model to show carriages details

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,7 +1,19 @@
 class Train < ActiveRecord::Base
-  validates :number, presence: true
-
   belongs_to :route
   has_many :tickets
   has_many :carriages
+
+  validates :number, presence: true
+
+  def cars_by_cat(category)
+    carriages.where(category: category)
+  end
+
+  def upper(category)
+    carriages.where(category: category).to_a.inject(0){ |sum, c| sum + c.upper }
+  end
+
+  def lower(category)
+    carriages.where(category: category).to_a.inject(0){ |sum, c| sum + c.lower }
+  end
 end
